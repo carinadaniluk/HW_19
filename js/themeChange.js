bacb24aedf7ac2c40e28ad731c227eb445f484de
carinadaniluk <164891027+carinadaniluk@users.noreply.github.com>
2024-09-02 23:19:25 -0400

    Update themeChange.js
    adding scroll to top

--- js/themeChange.js
+++ js/themeChange.js
@@ -6,3 +6,18 @@ $(".themeChange").on("click", function(){
         $("#stylesheet").attr("href", "./css/darkmode.css")
     }
 })
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+// dark mode
+$(".themeChange").on("click", function(){
+    if ($(this).hasClass("light-mode")) {
+        $("#stylesheet").attr("href", "./css/style.css")
+    }
+    else if ($(this).hasClass("dark-mode")) {
+        $("#stylesheet").attr("href", "./css/darkmode.css")
+    }
+})
