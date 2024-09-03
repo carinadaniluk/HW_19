bdebabc9dcaf7c87c7f13766cd72393ab9e026ea
carinadaniluk <164891027+carinadaniluk@users.noreply.github.com>
2024-09-02 22:51:21 -0400

    Update themeChange.js
    adding w3schools javascript scroll to top

--- js/themeChange.js
+++ js/themeChange.js
@@ -1,8 +1,19 @@
-$(".themeChange").on("click", function(){
-    if ($(this).hasClass("light-mode")) {
-        $("#stylesheet").attr("href", "./css/style.css")
-    }
-    else if ($(this).hasClass("dark-mode")) {
-        $("#stylesheet").attr("href", "./css/darkmode.css")
-    }
-})
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
